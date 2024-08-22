588ec2719170f6bd33e60c63345b876ab517a765
Yuefii <muffidahmadiawan31@gmail.com>
2024-08-22 16:51:34 +0700

    chore: adding docstring

--- app/config.py
+++ app/config.py
@@ -5,5 +5,12 @@ load_dotenv()
 
 
 class Config:
+    """
+    Konfigurasi untuk koneksi ke database.
+
+    Attributes:
+        SQLALCHEMY_DATABASE_URI (str): URL untuk koneksi pada database yang diambil dari variabel "DATABASE_URL" yang ada di .env.
+        SQLALCHEMY_TRACK_MODIFICATIONS (bool): Menonaktifkan pelacakan pada perubahan object untuk mencegah terjadinya overhead.
+    """
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/models.py
+++ app/models.py
@@ -2,5 +2,12 @@ from app import db
 
 
 class Provinces(db.Model):
+    """
+    Model untuk table "provinces" dalam database.
+
+    Attributes:
+        code (str): Code Unique untuk Provinsi (primary key).
+        name (str): Nama Provinsi yang tidak boleh kosong.
+    """
     code = db.Column(db.String(10), primary_key=True)
     name = db.Column(db.String(255), nullable=False)
