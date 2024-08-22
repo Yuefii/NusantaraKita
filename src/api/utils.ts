6cef4fecc303979c8b0d250c4c2e2e4ec5c06cbb
Yuefii <muffidahmadiawan31@gmail.com>
2024-08-22 11:11:18 +0700

    chore: converting dataset to mysql

--- src/api/utils.ts
+++
@@ -1,9 +0,0 @@
-import csv from "csvtojson"
-import path from "path";
-
-export class Utils {
-    async readCSV(fileName: string) {
-        const filePath = path.join(__dirname, '../../data', fileName);
-        return await csv().fromFile(filePath);
-    }
-}
--- src/error/response-error.ts
+++
@@ -1,5 +0,0 @@
-export class ResponseError extends Error {
-    constructor(public status: number, public message: string) {
-        super(message);
-    }
-}
