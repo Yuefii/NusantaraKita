42f22b52568208c957b76ba3922253d661049efa
Yuefii <muffidahmadiawan31@gmail.com>
2024-08-23 18:38:21 +0700

    chore: changes response json

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ province_bp = Blueprint("provinces", __name__)
 @province_bp.route("/api/provinces", methods=["GET"])
 def get_provinces():
     provinces = Provinces.query.all()
-    return jsonify(
-        [{"code": province.code, "name": province.name} for province in provinces]
-    )
+    response = [
+        {"code": province.code, "name": province.name} for province in provinces
+    ]
+    return jsonify({"data": response})
