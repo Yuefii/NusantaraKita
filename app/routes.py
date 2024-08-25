9803e912cc3982adb53a11407e3649e01469b4dd
Yuefii <muffidahmadiawan31@gmail.com>
2024-08-25 12:00:39 +0700

    chore: adding pagination on regencies

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,7 @@ def get_provinces():
         total_pages = (total_provinces + per_page - 1) // per_page
 
         if page > total_pages:
-            return jsonify({"error": "page number exceeds total pages"}), 400
+            return jsonify({"error": "Page number exceeds total pages"}), 400
 
         provinces_query = Provinces.query.paginate(
             page=page, per_page=per_page, error_out=False
@@ -49,14 +49,48 @@ def get_provinces():
 
 @regency_bp.route("/api/regencies", methods=["GET"])
 def get_regencies():
+    show_all = request.args.get("show_all", "false").lower() == "true"
+    page = request.args.get("page", 1, type=int)
+    per_page = request.args.get("per_page", 10, type=int)
     province_code = request.args.get("province_code")
-    if not province_code:
-        return jsonify({"error": "province code is required"}), 400
-    regencies = Regencies.query.filter_by(province_code=province_code).all()
-    if not regencies:
-        return jsonify({"message": "no regencies found for given province code"}), 404
-    response = [{"code": regency.code, "name": regency.name} for regency in regencies]
-    return jsonify({"data": response})
+
+    if show_all:
+        if province_code:
+            regencies = Regencies.query.filter_by(province_code=province_code).all()
+        else:
+            regencies = Regencies.query.all()
+        response = [
+            {"code": regency.code, "name": regency.name} for regency in regencies
+        ]
+    else:
+        if province_code:
+            total_regencies = Regencies.query.filter_by(
+                province_code=province_code
+            ).count()
+            regencies_query = Regencies.query.filter_by(
+                province_code=province_code
+            ).paginate(page=page, per_page=per_page, error_out=False)
+        else:
+            total_regencies = Regencies.query.count()
+            regencies_query = Regencies.query.paginate(
+                page=page, per_page=per_page, error_out=False
+            )
+        total_pages = (total_regencies + per_page - 1) // per_page
+        if page > total_pages:
+            return jsonify({"error": "page number exceeds total pages"}), 400
+        response = {
+            "pagination": {
+                "total_items": regencies_query.total,
+                "total_pages": regencies_query.pages,
+                "current_page": regencies_query.page,
+                "per_page": regencies_query.per_page,
+            },
+            "data": [
+                {"code": regency.code, "name": regency.name}
+                for regency in regencies_query.items
+            ],
+        }
+    return jsonify(response)
 
 
 @district_bp.route("/api/districts", methods=["GET"])
