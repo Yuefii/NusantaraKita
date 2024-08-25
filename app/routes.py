40ff40a3883a09f3a141d70918851a388adf7453
Yuefii <muffidahmadiawan31@gmail.com>
2024-08-25 14:16:18 +0700

    chore: adding pagination on villages

--- app/routes.py
+++ app/routes.py
@@ -137,16 +137,51 @@ def get_districts():
             ],
         }
 
-    return jsonify({"data": response})
+    return jsonify(response)
 
 
 @village_bp.route("/api/villages", methods=["GET"])
 def get_villages():
+    show_all = request.args.get("show_all", "false").lower() == "true"
+    page = request.args.get("page", 1, type=int)
+    per_page = request.args.get("per_page", 10, type=int)
     district_code = request.args.get("district_code")
-    if not district_code:
-        return jsonify({"error": "district code is required"}), 400
-    villages = Villages.query.filter_by(district_code=district_code).all()
-    if not villages:
-        return jsonify({"message": "no villages found for given district code"}), 404
-    response = [{"code": village.code, "name": village.name} for village in villages]
-    return jsonify({"data": response})
+
+    if show_all:
+        if district_code:
+            villages = Villages.query.filter_by(district_code=district_code).all()
+        else:
+            villages = Villages.query.all()
+        response = [
+            {"code": village.code, "name": village.name} for village in villages
+        ]
+    else:
+        if district_code:
+            total_villages = Villages.query.filter_by(
+                district_code=district_code
+            ).count()
+            villages_query = Villages.query.filter_by(
+                district_code=district_code
+            ).paginate(page=page, per_page=per_page, error_out=False)
+        else:
+            total_villages = Villages.query.count()
+            villages_query = Villages.query.paginate(
+                page=page, per_page=per_page, error_out=False
+            )
+        total_pages = (total_villages + per_page - 1) // per_page
+        if page > total_pages:
+            return jsonify({"error": "page number exceeds total pages"}), 400
+        response = {
+            "pagination": {
+                "total_items": villages_query.total,
+                "total_pages": villages_query.pages,
+                "current_page": villages_query.page,
+                "per_page": villages_query.per_page,
+            },
+            "data": [
+                {"code": village.code, "name": village.name}
+                for village in villages_query.items
+            ],
+        }
+
+    return jsonify(response)
